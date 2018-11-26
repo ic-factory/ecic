83e992db832bb6543dd5e644e47902a1609cbddc
Torben Jacobsen <nettet@foxware.dk>
2018-11-26 21:58:14 +0100

    removed debug print statement, since it was not formatted correctly GNU Make syntax (as required for the output)

--- lib/ecic/library.rb
+++ lib/ecic/library.rb
@@ -6,33 +6,32 @@ module Ecic
     attr_accessor :source_files
     attr_reader   :type
 
-    
     def initialize(project, name, type, options={})
       opt = {:path => nil}.merge(options)
       @project = project
-      @type = type
       @name = name
+      @type = type
       default_path = {:testbench => "src/testbench/#{@name}",
                       :design    => "src/design/#{@name}"}
       @path = opt[:path] || default_path[@type]
       @source_files = []
     end
-    
+
     def is_valid?
       begin
         validate_name
         return false if already_exists?
         #TBA: validate unique name as well
         return true
-        #TBA: 'validate_name' should raise a specific naming exception and only this specific exception should be caught. 
+        #TBA: 'validate_name' should raise a specific naming exception and only this specific exception should be caught.
       rescue Exception => exc
         return false
       end
     end
-    
+
     def create
       validate_name
-      @project.add_library self      
+      @project.add_library self
     end
 
     def already_exists?
@@ -42,7 +41,7 @@ module Ecic
     def to_str(options={})
       to_s(options)
     end
-    
+
     def to_s(options={})
       str = name
       incl_src_files = options[:include_source_files] || false
@@ -64,14 +63,12 @@ module Ecic
       src_file = File.join(@project.root, @path, 'sources.rb')
       if File.exists?(src_file)
         begin
-          puts "reading #{src_file} ..."
+          #puts "\#Reading #{src_file} ..."
           eval File.read(src_file)
         rescue Exception => exc
           raise "Syntax error occurred while reading #{src_file}: #{exc.message}"
         end
       else
-#        p @path
-#        p @type
         raise "Could not read sources for #{name} library. #{src_file} file does not exist"
       end
     end
@@ -79,7 +76,7 @@ module Ecic
     def is_a_testbench?
       @type == :testbench
     end
-    
+
     #Function used in sources.rb file of each library
     def source_file(path)
 #      puts "Creating new source file"
@@ -88,13 +85,13 @@ module Ecic
       new_src
     end
 
-    ###########################################################################    
+    ###########################################################################
     protected
-    ###########################################################################    
+    ###########################################################################
 
     def validate_name
       raise "#{@name} is not a valid library name. Library names must start with a letter, followed by one or more numbers, letters or underscores" unless /\A[a-zA-Z]\w*\Z/.match(@name)
     end
-    
+
   end
 end
